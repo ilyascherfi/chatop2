4f0ff8f4ab90347848d45ceebf1905f2a3d1432e
ilyas cherfi <ilyascherfi91@gmail.com>
2024-12-17 19:41:37 +0100

    initiate UserController

---
+++ src/main/java/com/openclassrooms/chatop/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.openclassrooms.chatop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.openclassrooms.chatop.model.User;
+import com.openclassrooms.chatop.service.UserService;
+
+
+@RestController
+public class UserController {
+
+	@Autowired
+    private UserService userService;
+
+    
+    @GetMapping("/users")
+    public Iterable<User> getUser() {
+        return userService.getUsers();
+    }
+}
+
+
+
+
