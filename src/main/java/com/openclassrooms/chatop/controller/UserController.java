a38e637934c50ab66b56f991b9529e2a7bb3ea3b
ilyas cherfi <ilyascherfi91@gmail.com>
2024-12-18 08:07:58 +0100

    add register methode to save user

--- src/main/java/com/openclassrooms/chatop/controller/UserController.java
+++ src/main/java/com/openclassrooms/chatop/controller/UserController.java
@@ -2,10 +2,13 @@ package com.openclassrooms.chatop.controller;
 
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.openclassrooms.chatop.model.User;
@@ -32,6 +35,11 @@ public class UserController {
     @GetMapping("/auth/me")
     public User getCurrentUser() {
     	return userService.getCurrentUser();
+    }
+    
+    @PostMapping("/auth/register")
+    public User registerUser(@RequestBody User user) {
+        return userService.saveUser(user);
     }
     
 }
