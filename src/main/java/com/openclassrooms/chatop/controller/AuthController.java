614605cd23b21889518faac5ad0de35710120c8c
ilyas cherfi <ilyascherfi91@gmail.com>
2025-01-07 11:13:08 +0100

    edit JWTService and try to create AuthController

--- src/main/java/com/openclassrooms/chatop/controller/AuthController.java
+++ src/main/java/com/openclassrooms/chatop/controller/AuthController.java
@@ -2,12 +2,17 @@ package com.openclassrooms.chatop.controller;
 
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.openclassrooms.chatop.dto.request.LoginRequest;
 import com.openclassrooms.chatop.dto.request.RegisterRequest;
+import com.openclassrooms.chatop.dto.response.JwtResponse;
 import com.openclassrooms.chatop.dto.response.RegisterResponse;
 import com.openclassrooms.chatop.model.User;
 import com.openclassrooms.chatop.service.JWTService;
@@ -17,21 +22,31 @@ import com.openclassrooms.chatop.service.UserService;
 @RequestMapping("/api")
 public class AuthController {
 
-	
+
 	public JWTService jwtService;
 	private UserService userService; 
+	private AuthenticationManager authenticationManager;
 	
 	@Autowired
-	public AuthController(UserService userService, JWTService jwtService) {
+	public AuthController(AuthenticationManager authenticationManager, UserService userService, JWTService jwtService) {
 		this.jwtService = jwtService;
 		this.userService = userService;
+		this.authenticationManager = authenticationManager;
 	}
 	
 	@PostMapping("/auth/login") 
-	public String getToken(Authentication authentication) {
-		String token = jwtService.generateToken(authentication);
-		return token;
-	}
+	public JwtResponse authenticateUser(@RequestBody LoginRequest loginRequest) {
+        // Authentifie l'utilisateur avec les identifiants fournis
+        Authentication authentication = authenticationManager.authenticate(
+        		new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+        		
+        // Récupère les détails de l'utilisateur après authentification
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String token = jwtService.generateJwtToken(userDetails.getUsername());
+
+        // Renvoie le token dans une réponse structurée
+        return new JwtResponse(token);
+    }
 	
 	@PostMapping("/auth/register")
 	public RegisterResponse registerUser(@RequestBody RegisterRequest registerRequest) {
--- src/main/java/com/openclassrooms/chatop/service/JWTService.java
+++ src/main/java/com/openclassrooms/chatop/service/JWTService.java
@@ -1,8 +1,10 @@
 package com.openclassrooms.chatop.service;
 
-
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwsHeader;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
@@ -13,23 +15,32 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JWTService {
-
-
 	private JwtEncoder jwtEncoder;
-	
-	public JWTService(JwtEncoder jwtEncoder) {
-		this.jwtEncoder = jwtEncoder;
-	}
-	
-	public String generateToken(org.apache.tomcat.util.net.openssl.ciphers.Authentication authentication) {
-        		Instant now = Instant.now();
-     		JwtClaimsSet claims = JwtClaimsSet.builder()
-              		  .issuer("self")
-               		  .issuedAt(now)
-              		  .expiresAt(now.plus(1, ChronoUnit.DAYS))
-              		  .subject(authentication.name())
-              		  .build();
-		JwtEncoderParameters jwtEncoderParameters = JwtEncoderParameters.from(JwsHeader.with(MacAlgorithm.HS256).build(), claims);
-		return this.jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
-	}
+    private AuthenticationManager authenticationManager;
+
+    public JWTService(JwtEncoder jwtEncoder, AuthenticationManager authenticationManager) {
+        this.jwtEncoder = jwtEncoder;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public String generateToken(Authentication authentication) {
+        Instant now = Instant.now();
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plus(1, ChronoUnit.DAYS))
+                .subject(authentication.getName())
+                .build();
+        JwtEncoderParameters jwtEncoderParameters = JwtEncoderParameters.from(JwsHeader.with(MacAlgorithm.HS256).build(), claims);
+        return this.jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
+    }
+
+    public String authenticate(String email, String password){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+        if (authentication.isAuthenticated()){
+            String token = generateToken(authentication);
+            return token;
+        }
+        else return "";
+    }
 }
