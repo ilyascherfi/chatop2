9dbe8252243a2c12acabb9ce654c06bf5570b2bf
ilyas cherfi <ilyascherfi91@gmail.com>
2025-01-12 13:58:49 +0100

    fix user model

--- src/main/java/com/openclassrooms/chatop/model/User.java
+++ src/main/java/com/openclassrooms/chatop/model/User.java
@@ -1,6 +1,8 @@
 package com.openclassrooms.chatop.model;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.*;
 
@@ -11,74 +13,21 @@ import lombok.Data;
 @Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class User {
 	
-	public User(String username, String email, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
-	
-	public User() {
-    }
-
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-	
 
-    @Column(nullable = false, unique = true)
     private String email;
-
-    @Column(nullable = false)
-    private String username;
-
-    @Column(nullable = false)
+    private String name;
     private String password;
 
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    @Column(name = "created_at", updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd")
+    private LocalDate created_at;
 
     @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
-
-    
-    @PrePersist
-    protected void onCreate() {
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        this.updatedAt = LocalDateTime.now();
-    }
-    
-    public String getUsername() {
-        return username;
-    }
-    
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-	public Long getId() {
-		return id;
-	}
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd")
+    private LocalDate updated_at;
 	
 	
 }
